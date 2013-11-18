8ae54ea911bf2b18a49de62cfe8d02ef1a63a4ab
Rany Keddo <rany@soundcloud.com>
2013-11-18 19:17:17 +0100

    Moved delayed bandit tests
    Now in bandit_test.go to reflect that delayed bandit code lives in bandit.go.

--- bandit.go
+++ bandit.go
@@ -211,6 +211,52 @@ func (u *uCB1) Version() string {
 	return fmt.Sprintf("UCB1")
 }
 
+// NewDelayedBandit wraps the given bandit.
+func NewDelayedBandit(b Bandit, updates chan Counters) (Bandit, error) {
+	bandit := delayedBandit{
+		bandit:  b,
+		updates: updates,
+	}
+
+	go func() {
+		for counters := range updates {
+			b.Init(&counters)
+		}
+	}()
+
+	return &bandit, nil
+}
+
+// delayedBandit wraps a bandit. Internal counters are stored at the
+// configured source file, which is pooled at `poll` interval. The retrieved
+// Snapshot replaces the bandit's internal counters.
+type delayedBandit struct {
+	Counters
+	updates chan Counters
+	bandit  Bandit
+}
+
+// SelectArm delegates to the wrapped bandit
+func (b *delayedBandit) SelectArm() int {
+	return b.bandit.SelectArm()
+}
+
+// Version gives information about delayed bandit + the wrapped bandit.
+func (b *delayedBandit) Version() string {
+	return fmt.Sprintf("Delayed(%s)", b.bandit.Version())
+}
+
+// DelayedUpdate updates the internal counters of a bandit with the provided
+// counters.
+func (b *delayedBandit) Init(c *Counters) error {
+	b.Lock()
+	defer b.Unlock()
+	return b.bandit.Init(c)
+}
+
+// Update is a NOP. Delayed bandit is updated with Reset(counter) instead
+func (b *delayedBandit) Update(arm int, reward float64) {}
+
 // Counters maintain internal bandit state
 type Counters struct {
 	sync.Mutex
@@ -266,49 +312,3 @@ func (c *Counters) Reset() {
 	c.counts = make([]int, c.arms)
 	c.values = make([]float64, c.arms)
 }
-
-// NewDelayedBandit wraps the given bandit.
-func NewDelayedBandit(b Bandit, updates chan Counters) (Bandit, error) {
-	bandit := delayedBandit{
-		bandit:  b,
-		updates: updates,
-	}
-
-	go func() {
-		for counters := range updates {
-			b.Init(&counters)
-		}
-	}()
-
-	return &bandit, nil
-}
-
-// delayedBandit wraps a bandit. Internal counters are stored at the
-// configured source file, which is pooled at `poll` interval. The retrieved
-// Snapshot replaces the bandit's internal counters.
-type delayedBandit struct {
-	Counters
-	updates chan Counters
-	bandit  Bandit
-}
-
-// SelectArm delegates to the wrapped bandit
-func (b *delayedBandit) SelectArm() int {
-	return b.bandit.SelectArm()
-}
-
-// Version gives information about delayed bandit + the wrapped bandit.
-func (b *delayedBandit) Version() string {
-	return fmt.Sprintf("Delayed(%s)", b.bandit.Version())
-}
-
-// DelayedUpdate updates the internal counters of a bandit with the provided
-// counters.
-func (b *delayedBandit) Init(c *Counters) error {
-	b.Lock()
-	defer b.Unlock()
-	return b.bandit.Init(c)
-}
-
-// Update is a NOP. Delayed bandit is updated with Reset(counter) instead
-func (b *delayedBandit) Update(arm int, reward float64) {}
--- bandit_test.go
+++ bandit_test.go
@@ -181,3 +181,46 @@ func TestUCB1(t *testing.T) {
 		t.Fatalf("cumulative performance should be > %f. is %f", expectedCumulative, got)
 	}
 }
+
+func TestDelayedBandit(t *testing.T) {
+	τ := 0.1
+	sims := 5000
+	trials := 300
+	flushAfter := 100
+	bestArmIndex := 4 // Bernoulli(bestArm)
+	bestArm := 0.8
+	arms := []sim.Arm{
+		sim.Bernoulli(0.1),
+		sim.Bernoulli(0.3),
+		sim.Bernoulli(0.2),
+		sim.Bernoulli(0.8),
+	}
+
+	b, err := NewSoftmax(len(arms), τ)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	d := NewSimulatedDelayedBandit(b, len(arms), flushAfter)
+
+	s, err := sim.MonteCarlo(sims, trials, arms, d)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	accuracies := sim.Accuracy([]int{bestArmIndex})(&s)
+	if got := accuracies[len(accuracies)-1]; got < 0.9 {
+		t.Fatalf("accuracy is only %f. %d sims, %d trials", got, sims, trials)
+	}
+
+	performances := sim.Performance(&s)
+	if got := performances[len(performances)-1]; math.Abs(bestArm-got) > 0.1 {
+		t.Fatalf("performance converge to %f. is %f", bestArm, got)
+	}
+
+	expectedCumulative := 200.0
+	cumulatives := sim.Cumulative(&s)
+	if got := cumulatives[len(cumulatives)-1]; got < expectedCumulative {
+		t.Fatalf("cumulative performance should be > %f. is %f", expectedCumulative, got)
+	}
+}
